0406c45c640ae2a91be6f5fdd578fcaf7a9eeb65
SanchitTaliyan <sanchitt@maqsoftware.com>
2019-06-11 18:34:41 +0530

    Added Select User Functionality

--- launcher/app/src/main/java/com/maq/xprize/kitkitlauncher/hindi/MultiUserActivity.java
+++ launcher/app/src/main/java/com/maq/xprize/kitkitlauncher/hindi/MultiUserActivity.java
@@ -1,13 +1,16 @@
 package com.maq.xprize.kitkitlauncher.hindi;
 
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,12 +20,15 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.maq.kitkitProvider.KitkitDBHandler;
 import com.maq.kitkitProvider.User;
 
 import java.io.ByteArrayOutputStream;
+import java.net.URI;
 import java.util.ArrayList;
 
 public class MultiUserActivity extends AppCompatActivity {
@@ -38,10 +44,16 @@ public class MultiUserActivity extends AppCompatActivity {
     Button close;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private byte imageInByte[];
-
+    private Uri picUri;
+    static final int PIC_CROP = 2;
     private Context schoolContext;
     private SharedPreferences schoolPref;
     private UserNameListDialog userNameListDialog;
+    Dialog selectUserDialog;
+    private ViewPager imagePager;
+    Button exit;
+    ImageView goToDashboard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +73,10 @@ public class MultiUserActivity extends AppCompatActivity {
         addUserDialog = new Dialog(this);
         usrname = (TextView)findViewById(R.id.tv_name);
 
+        selectUserDialog = new Dialog(this);
+        selectUserDialog.setContentView(R.layout.activity_select_user);
+        imagePager = (ViewPager) selectUserDialog.findViewById(R.id.viewpager);
+
     }
 
 
@@ -68,22 +84,55 @@ public class MultiUserActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
-            if(requestCode == REQUEST_IMAGE_CAPTURE){
+            if(requestCode == REQUEST_IMAGE_CAPTURE) {
+               // picUri = data.getData();
+               // performCrop();
+            //}
+      //  else if(requestCode == PIC_CROP) {                                                      //////////////////////////////
                 Bundle extras = data.getExtras();
 
-                if(extras != null){
+                if (extras != null) {
                     Bitmap yourImage = extras.getParcelable("data");
 
                     //convert Bitmap to byte
 
-                    ByteArrayOutputStream  stream = new ByteArrayOutputStream();
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
                     yourImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
                     imageInByte = stream.toByteArray();
                     Log.e("out before conversion", imageInByte.toString());
                 }
-            }
+            }                                                                                  /////////////////////////////
         }
     }
+
+    // function to perform crop
+    ///////////////////////////////////
+    private void performCrop() {
+        try {
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            //indicate image type and Uri
+            cropIntent.setDataAndType(picUri, "image/*");
+            //set crop properties
+            cropIntent.putExtra("crop", "true");
+            //indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 0);
+            cropIntent.putExtra("aspectY", 0);
+            //indicate output X and Y
+            cropIntent.putExtra("outputX", 200);
+            cropIntent.putExtra("outputY", 200);
+            //retrieve data on return
+            cropIntent.putExtra("return-data", true);
+            //start the activity - we handle returning in onActivityResult
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        catch(ActivityNotFoundException anfe){
+            //display an error message
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+                                   ///////////////////////////////
 
     public void AddAllUser(View v) {
 
@@ -116,12 +165,15 @@ public class MultiUserActivity extends AppCompatActivity {
 
                 Intent takepic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(takepic, REQUEST_IMAGE_CAPTURE);
+
+
                 takepic.setType("image/*");
                 takepic.putExtra("crop", "true");
                 takepic.putExtra("aspectX", 0);
                 takepic.putExtra("aspectY", 0);
                 takepic.putExtra("outputX", 250);
                 takepic.putExtra("outputY", 200);
+
             }
         });
 
@@ -172,6 +224,61 @@ public class MultiUserActivity extends AppCompatActivity {
             userNameListDialog.show();
         }
     }
+
+
+    public void SelectUser(View view) {
+        try {
+            KitkitDBHandler dbHandler = ((LauncherApplication) getApplication()).getDbHandler();
+            ArrayList<User> users = dbHandler.getUserList();
+            exit = (Button) selectUserDialog.findViewById(R.id.close);
+            goToDashboard = (ImageView) selectUserDialog.findViewById(R.id.gotodashboard);
+/*
+            schoolContext = getApplicationContext().createPackageContext("com.maq.xprize.kitkitschool.hindi", 0);
+            schoolPref = schoolContext.getSharedPreferences("Cocos2dxPrefsFile", Context.MODE_PRIVATE);
+            for (User u:users) {
+                u.setGamesClearedInTotal_L(schoolPref.getInt((u.getUserName() + "_gamesClearedInTotal_en-US_L"), 0));
+                u.setGamesClearedInTotal_M(schoolPref.getInt((u.getUserName() + "_gamesClearedInTotal_en-US_M"), 0));
+            }
+            */
+
+            /*
+            goToDashboard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        KitkitDBHandler dbHandler = new KitkitDBHandler(getApplicationContext());
+                    }
+                }
+            });
+            */
+
+
+            exit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectUserDialog.dismiss();
+                }
+            });
+            selectUserDialog.show();
+            imagePager.setAdapter(new SlidingPagerAdapter(MultiUserActivity.this, users));
+        }
+        catch (Exception ne) {
+            Log.e(TAG, ne.toString());
+            imagePager.setAdapter(new SlidingPagerAdapter(MultiUserActivity.this, null));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void SetUser(View view) {
         KitkitDBHandler dbHandler = ((LauncherApplication) getApplication()).getDbHandler();
---
+++ launcher/app/src/main/java/com/maq/xprize/kitkitlauncher/hindi/SlidingPagerAdapter.java
@@ -0,0 +1,80 @@
+package com.maq.xprize.kitkitlauncher.hindi;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.maq.kitkitProvider.KitkitDBHandler;
+import com.maq.kitkitProvider.User;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+
+public class SlidingPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private ArrayList<User> mData;
+    private LayoutInflater layoutInflater;
+
+
+    public SlidingPagerAdapter(@NonNull Context context, ArrayList<User> users) {
+
+        mContext = context;
+        mData = users;
+
+        if (mData != null && mData.get(0).getUserName().equals("user0")) {
+            User user0 = mData.remove(0);
+            mData.add(user0);
+        }
+
+
+
+    }
+
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        User item = mData.get(position);
+        layoutInflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.sliding_images,container,false);
+        ImageView imageView = (ImageView) view.findViewById(R.id.finaluserimage);
+        TextView english = (TextView) view.findViewById(R.id.tv_eng);
+        TextView math = (TextView) view.findViewById(R.id.tv_maths);
+
+        imageView.setImageBitmap(converToBitmap(item.getImage()));
+        english.setText(String.valueOf(item.getGamesClearedInTotal_L()));
+        math.setText(String.valueOf(item.getGamesClearedInTotal_M()));
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ConstraintLayout)object);
+    }
+
+    public Bitmap converToBitmap(byte[] image){
+        ByteArrayInputStream imageStream = new ByteArrayInputStream(image);
+        Bitmap theImage = BitmapFactory.decodeStream(imageStream);
+        return theImage;
+    }
+
+}
