1dc57f666fbed9b0c176826ecfe3c4d412b5b9af
Aakash Garg <aakashga@maqsoftware.com>
2019-06-29 19:55:38 +0530

    Imported all the necessary files of Library into Bookviewer

--- bookviewer/proj.android-studio/app/src/org/cocos2dx/cpp/BookApplication.java
+++ bookviewer/proj.android-studio/app/src/org/cocos2dx/cpp/BookApplication.java
@@ -1,8 +1,14 @@
 package org.cocos2dx.cpp;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.maq.kitkitlogger.KitKitLogger;
+import com.nostra13.universalimageloader.BuildConfig;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 /**
  * Created by ingtellect on 7/25/17.
@@ -19,7 +25,7 @@ public class BookApplication extends Application {
         logger = new KitKitLogger(getPackageName(), getApplicationContext());
 
         defaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
-
+        initImageLoader(this);
         // Setup handler for uncaught exceptions.
         Thread.setDefaultUncaughtExceptionHandler (new Thread.UncaughtExceptionHandler()
         {
@@ -42,5 +48,24 @@ public class BookApplication extends Application {
 
     public KitKitLogger getLogger() {
         return logger;
+    }
+    public static void initImageLoader(Context context) {
+        // This configuration tuning is custom. You can tune every option, you may tune some of them,
+        // or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        // method.
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(100 * 1024 * 1024); // 100 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+
+        if (BuildConfig.DEBUG == true) {
+            config.writeDebugLogs(); // Remove for release app
+        }
+
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
     }
 }
