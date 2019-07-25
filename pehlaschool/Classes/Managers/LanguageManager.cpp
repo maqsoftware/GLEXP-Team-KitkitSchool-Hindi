4d28b404665f5b9b66fed81b665f21aedf85cf81
Shailin Shah <shailins@maqsoftware.com>
2019-07-25 19:18:25 +0530

    Resolved PR comments

--- pehlaschool/Classes/Managers/LanguageManager.cpp
+++ pehlaschool/Classes/Managers/LanguageManager.cpp
@@ -226,17 +226,9 @@ void LanguageManager::initLocalizationMap()
     _localizationMapSwahili["Great!"] = "Vizuri!";
 
     _localizationMapEnglish["Are you ready for"] = "Are you ready for"; //
-    // _localizationMapHindi["Are you ready for"] = "क्या तुम तैयार हो";
+     _localizationMapHindi["Are you ready for"] = "क्या तुम तैयार हो";
     // _localizationMapUrdu["Are you ready for"] = "";
     _localizationMapBengali["Are you ready for"] = "আপনি পরীক্ষা জন্য প্রস্তুত?";
-
-    // _localizationMapBengali["Great!"] = "";
-    _localizationMapSwahili["Great!"] = "Vizuri!";
-
-    _localizationMapEnglish["Are you ready for"] = "Are you ready for";
-    _localizationMapHindi["Are you ready for"] = "क्या तुम तैयार हो";
-    // _localizationMapUrdu["Are you ready for"] = "";
-    // _localizationMapBengali["Are you ready for"] = "";
     _localizationMapSwahili["Are you ready for"] = "Je, uko tayari kwa";
 
     _localizationMapEnglish["Prove it!"] = "Prove it!";
@@ -539,7 +531,7 @@ void LanguageManager::initLocalizationMap()
     _localizationMapSwahili["NumberPuzzle"] = "Fumbo la Nambari";
 
     _localizationMapEnglish["Arrange the numbers in order from smallest to largest"] = "Arrange the numbers in order from smallest to largest.";
-    // _localizationMapHindi["Arrange the numbers in order from smallest to largest"] = "छोटी से लेकर बड़ी अंक के क्रम में संख्याओं की क्रमबद्ध करें।";
+    _localizationMapHindi["Arrange the numbers in order from smallest to largest"] = "छोटी से लेकर बड़ी अंक के क्रम में संख्याओं की क्रमबद्ध करें।";
     // _localizationMapUrdu["Arrange the numbers in order from smallest to largest"] = "";
     _localizationMapBengali["Arrange the numbers in order from smallest to largest"] = "ছোট থেকে বৃহত্তম করার জন্য সংখ্যা সাজান";
     _localizationMapSwahili["Arrange the numbers in order from smallest to largest"] = "panga kwa mpangilio kutoka ndogo zaidi kwenda kubwa zaidi";
